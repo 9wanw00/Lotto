540e86e3bca972e4ebc4d824e72169a7ce8a09e2
gwanwoo <ppkw2001@gmail.com>
2024-04-24 14:41:44 +0900

    first commit

---
+++ src/main/kotlin/org/example/lotto/LottoApplication.kt
@@ -0,0 +1,11 @@
+package org.example.lotto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LottoApplication
+
+fun main(args: Array<String>) {
+	runApplication<LottoApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/lotto/LottoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.lotto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LottoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
